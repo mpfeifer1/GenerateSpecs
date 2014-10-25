17ae7e0b0825c14082916e7390034b2224b70361
mpfeifer <mikepf97@gmail.com>
2014-10-25 17:55:34 -0500

    Reorganized code

--- GenetareSpecs.sh
+++ GenetareSpecs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Delete existing spec sheet, if any
 rm /home/$USER/Documents/Specs_$HOSTNAME.txt
 
 #Allows you to print to spec sheet easily
@@ -8,38 +9,36 @@ function print_line()
 echo $1 $2 $3>> /home/$USER/Documents/Specs_$HOSTNAME.txt
 }
 
-#Print First Lines
+#OS
+KERNEL=$(uname -or)
+ARCH=$(uname -i)
+
+#RAM
+RAM=$(vmstat -s | head -n 1 |  awk -F' ' '{print $1}')
+
+#Screen Resolution
+SCREEN=$(xdpyinfo  | grep dimensions | awk -F' ' '{print $2}')
+
+#HDD Size
+HDD=$(lsblk | awk 'NR==2' | head -c 23 | tail -c 5)
+
+#CPU Info
+CORES=$(cat /proc/cpuinfo | awk 'NR==13' | cut -d ":" -f 2)
+MAKER=$(cat /proc/cpuinfo | awk 'NR==2'  | cut -d ":" -f 2)
+CLOCK=$(cat /proc/cpuinfo | awk 'NR==5'  | cut -d "@" -f 2)
+
+
+#Print Spec Sheet
 print_line $HOSTNAME Specs
 print_line $USERNAME
 print_line
-
-#Print OS
-KERNEL=$(uname -or)
 print_line "Linux Kernel =======>" $KERNEL
-
-ARCH=$(uname -i)
 print_line "Linux Architecture =>" $ARCH
-
 print_line
-
-#Print RAM
-RAM=$(vmstat -s | head -n 1 |  awk -F' ' '{print $1}')
 print_line "Total Ram ==========>" $RAM "Kb"
-
-#Print Screen Resolution
-SCREEN=$(xdpyinfo  | grep dimensions | awk -F' ' '{print $2}')
 print_line "Screen Res =========>" $SCREEN
-
-#Print HDD Size
-HDD=$(lsblk | awk 'NR==2' | head -c 23 | tail -c 5) #TODO
 print_line "HDD Size ===========>" $HDD Gb
-
 print_line
-
-#Print CPU Info
-CORES=$(cat /proc/cpuinfo | awk 'NR==13' | cut -d ":" -f 2)
 print_line "CPU Cores ==========>" $CORES
-MAKER=$(cat /proc/cpuinfo | awk 'NR==2' | cut -d ":" -f 2)
 print_line "CPU Manufacturer ===>" $MAKER
-CLOCK=$(cat /proc/cpuinfo | awk 'NR==5' | cut -d "@" -f 2)
 print_line "CPU Clock ==========>" $CLOCK
