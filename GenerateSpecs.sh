710c772ead4442ae1e8052cf91cd4222789aca09
mpfeifer <mikepf97@gmail.com>
2014-11-03 23:40:36 -0600

    De-commented, and added new organizational structure code

--- GenerateSpecs.sh
+++ GenerateSpecs.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
-
 #Delete existing spec sheet, if any
 rm -f /home/$USER/Documents/Specs_$HOSTNAME.txt
 
 #Allows you to print to spec sheet easily
-function print_line()
-{
+function print_line() {
 echo $1 $2 $3>> /home/$USER/Documents/Specs_$HOSTNAME.txt
+}
+
+function first_word() {
+
+}
+
+function after_symbol() {
+
+}
+
+function line() {
+
 }
 
 #OS
@@ -25,8 +34,8 @@ HDD=$(lsblk | awk 'NR==2' | head -c 23 | tail -c 5)
 #CPU Info
 MAKER=$(cat /proc/cpuinfo | awk 'NR==2'  | cut -d ":" -f 2)
 MODEL=$(cat /proc/cpuinfo | awk 'NR==5'  | cut -d ":" -f 2 | head -c 37 | tail -c 8) #fix in some way
-CORES=$(lscpu | awk 'NR==4' | cut -d ":" -f 2) #Change to lscpu?
-CLOCK=$(cat /proc/cpuinfo | awk 'NR==5'  | cut -d "@" -f 2) #Change to lscpu
+CORES=$(lscpu | awk 'NR==4' | cut -d ":" -f 2)
+CLOCK=$(lscpu | grep 'MHz'| cut -d ":" -f 2)
 
 
 #Print Spec Sheet
@@ -43,4 +52,4 @@ print_line
 print_line "CPU Manufacturer ===>" $MAKER
 print_line "CPU Model ==========>" $MODEL
 print_line "CPU Cores ==========>" $CORES
-print_line "CPU Clock ==========>" $CLOCK
+print_line "CPU Clock ==========>" $CLOCK "MHz"
