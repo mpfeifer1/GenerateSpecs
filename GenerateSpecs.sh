1e1c068cdef9fbc123f77e4f032bd86b1e1a1701
Michael Pfeifer <mikepf97@gmail.com>
2015-02-03 08:31:26 -0600

    Fixed clock speed?

--- GenerateSpecs.sh
+++ GenerateSpecs.sh
@@ -6,18 +6,6 @@ function print_line() {
 echo $1 $2 $3>> /home/$USER/Documents/Specs_$HOSTNAME.txt
 }
 
-#function first_word() {
-
-#}
-
-#function after_symbol() {
-
-#}
-
-#function line() {
-
-#}
-
 #OS
 KERNEL=$(uname -or)
 ARCH=$(uname -i)
@@ -33,9 +21,9 @@ HDD=$(lsblk | awk 'NR==2' | head -c 23 | tail -c 5)
 
 #CPU Info
 MAKER=$(cat /proc/cpuinfo | awk 'NR==2'  | cut -d ":" -f 2)
-MODEL=$(cat /proc/cpuinfo | awk 'NR==5'  | cut -d ":" -f 2 | head -c 37 | tail -c 8) #fix in some way
+MODEL=$(cat /proc/cpuinfo | awk 'NR==5'  | cut -d ":" -f 2)
 CORES=$(lscpu | awk 'NR==4' | cut -d ":" -f 2)
-CLOCK=$(lscpu | grep 'MHz'| cut -d ":" -f 2)
+CLOCK=$(cat /proc/cpuinfo | awk 'NR==5'  | cut -d ":" -f 2 | tail -c 8)
 
 
 #Print Spec Sheet
@@ -52,4 +40,4 @@ print_line
 print_line "CPU Manufacturer ===>" $MAKER
 print_line "CPU Model ==========>" $MODEL
 print_line "CPU Cores ==========>" $CORES
-print_line "CPU Clock ==========>" $CLOCK "MHz"
+print_line "CPU Clock ==========>" $CLOCK
